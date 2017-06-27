ebb4a5d5c7e881b8e9fd948b369b90c2d2e1a246
Jason Levinsohn <jason.levinsohn@gmail.com>
2017-06-27 16:05:36 -0400

    created base layout of app

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by jlev on 6/27/17.
+//  Copyright © 2017 L3. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    // Need this to initialize each pokemon object
+    init(name: String, pokedexId: Int) {
+        self._pokedexId = pokedexId
+        self._name = name
+    }
+}
